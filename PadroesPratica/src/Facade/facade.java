5edf1685b526380ce07b53035fcf0bac219a4051
Gabriel Ortiz <gabrielgomes96@outlook.com>
2024-05-21 14:45:47 -0300

    padroes-pratica

---
+++ PadroesPratica/src/Facade/facade.java
@@ -0,0 +1,11 @@
+package Facade;
+
+public class Facade {
+
+	public void mudarCliente(String nome, String cep) {
+		String cidade = CepApi.getInstancia().recuperarCidade(cep);
+		String estado = CepApi.getInstancia().recuperarEstado(cep);
+		
+		CrmService.gravarCliente(nome, cep, cidade, estado);
+	}
+}
